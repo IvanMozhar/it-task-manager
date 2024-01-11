5e7276d9d617f87a7019778e733cdcf4757d8afe
Ivan <mozharivan17@gmail.com>
2024-01-11 08:37:05 +0200

    Edit namespace in project urls

--- it_task_manager/urls.py
+++ it_task_manager/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("task_manager.urls", namespace="manager"))
+    path("", include("task_manager.urls", namespace="task_manager"))
 ]
