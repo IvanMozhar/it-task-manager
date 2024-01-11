d5d8023ad57e5f87d767ae17b023844ca9fec571
Ivan <mozharivan17@gmail.com>
2024-01-11 07:51:38 +0200

    Craete views for home page and for working with tasks

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from task_manager.forms import TaskNameSearchForm, TaskForm
+from task_manager.models import Task
+
+
+@login_required
+def index(request):
+    context = {"segment": "index"}
+    html_template = loader.get_template("home/index.html")
+    return HttpResponse(html_template.render(context, request))
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    template_name = "manager/task_list.html"
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = TaskNameSearchForm(initial={"name": name})
+        return context
+
+    def get_queryset(self):
+        form = TaskNameSearchForm(self.request.GET)
+        if form.is_valid():
+            return Task.objects.all().filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return Task.objects.all()
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("manager:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("manager:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("manager:task-list")
