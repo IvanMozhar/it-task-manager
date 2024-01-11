43c218f3eedfaf63a58817a73f04f1ba4e245079
Ivan <mozharivan17@gmail.com>
2024-01-11 07:57:47 +0200

    Craete views for working with TaskType model

--- task_manager/views.py
+++ task_manager/views.py
@@ -6,8 +6,8 @@ from django.template import loader
 from django.urls import reverse_lazy
 from django.views import generic
 
-from task_manager.forms import TaskNameSearchForm, TaskForm
-from task_manager.models import Task
+from task_manager.forms import TaskNameSearchForm, TaskForm, TaskTypeForm, TaskTypeNameSearchForm
+from task_manager.models import Task, TaskType
 
 
 @login_required
@@ -57,3 +57,39 @@ class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
 class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Task
     success_url = reverse_lazy("manager:task-list")
+
+
+class TaskTypeListView(LoginRequiredMixin, generic.ListView):
+    model = TaskType
+    context_object_name = "task_type_list"
+    template_name = "manager/task_type_list.html"
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskTypeListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = TaskNameSearchForm(initial={"name": name})
+        return context
+
+    def get_queryset(self):
+        form = TaskTypeNameSearchForm(self.request.GET)
+        if form.is_valid():
+            return TaskType.objects.all().filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return TaskType.objects.all()
+
+
+class TaskTypeDetailView(LoginRequiredMixin, generic.DetailView):
+    model = TaskType
+
+
+class TaskTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = TaskType
+    form_class = TaskTypeForm
+    success_url = reverse_lazy("manager:task-type-list")
+
+
+class TaskTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = TaskType
+    success_url = reverse_lazy("manager:task-type-list")
